3bfd8bcd281dcc0b98c6cbd03ab82029c959559e
will-dml <will_ou@outlook.fr>
2021-12-02 21:17:39 +0000

    Add structure to game functions

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function runGame() {
+
+
+}
+
+function checkAnswer(){
+
+
+}
+
+function calculateCorrectAnswer() {
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+
+}
+
+function displaySubstractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
